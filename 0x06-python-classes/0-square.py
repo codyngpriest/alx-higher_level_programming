cb5d3c99909cfb88251db78a9c0609222cb98da3
codyngpriest <vilhopriestly@gmail.com>
2024-04-02 07:16:56 +0000

    implement 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py
+# Vilho Banike <vilhopriestly@gmail.com>
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,6 @@
+0x06. Python - Classes and Objects
+
+Tasks
+
+0. My first square
+This is an empty class Square that defines a square:
