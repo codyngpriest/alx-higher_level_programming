db2d358b15acfe55b2a70ced97007a55b3205ceb
codyngpriest <vilhopriestly@gmail.com>
2024-07-30 06:57:41 +0000

    implement 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that prints the title of a Star Wars movie
+const myArray = process.argv.slice(2);
+const request = require('request');
+
+if (Number.isInteger(parseInt(myArray[0]))) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${myArray[0]}`;
+  request(apiUrl, function (error, response, body) {
+    if (error) console.error('error:', error);
+    console.log(JSON.parse(body).title);
+  });
+}
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -10,3 +10,6 @@ This is a script that writes a string to a file.
 
 2. Status code
 This is a script that display the status code of a GET request.
+
+3. Star wars movie title
+This is a script that prints the title of a Star Wars movie where the episode number matches a given integer.
