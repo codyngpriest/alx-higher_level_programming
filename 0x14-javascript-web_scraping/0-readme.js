625656a438ef7ad8e45f29214daec1bff31d550c
codyngpriest <vilhopriestly@gmail.com>
2024-07-30 06:51:41 +0000

    implement 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Reads file passed as an arguement
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  console.log(error || data);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,6 @@
+0x14. JavaScript - Web scraping
+
+Tasks
+
+0. Readme
+This is a script that reads and prints the content of a file.
